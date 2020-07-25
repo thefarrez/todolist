ad5d0edb0eb4883537645378525fb094ca515af2
Farhad Rezai <faradrezai@gmail.com>
2020-07-25 13:42:37 +0200

    Updates

---
+++ precache-manifest.ebb81aa61687f8c246a8be6c0d205b26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fa3af2c713cf75843265510ba5a9c6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a17d6421c83a8249c120",
+    "url": "/todolist/static/css/main.c2c8ae0d.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/todolist/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17d6421c83a8249c120",
+    "url": "/todolist/static/js/main.75686997.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
